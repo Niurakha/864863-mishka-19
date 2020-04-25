202187df2b7ecbd5c6c6e1a907fda103549d4c74
Niurakha <59962207+Niurakha@users.noreply.github.com>
2020-04-25 17:58:07 +0300

    Переписала js для выпадающего меню

--- source/js/dropdown.js
+++ source/js/dropdown.js
@@ -1,10 +1,17 @@
 "use strict";
 
-(function () {
-  let toggle = document.querySelector(".main-nav__toggle");
-  toggle.addEventListener("click", function (event) {
-    event.preventDefault();
-    let menu = document.querySelector(".main-nav");
-    menu.classList.toggle("is-open");
-  });
-})();
+let navToggle = document.querySelector(".main-nav__toggle");
+let navMain = document.querySelector(".main-nav");
+navMain.classList.remove("main-nav--nojs");
+navMain.classList.remove("main-nav--opened");
+navMain.classList.add("main-nav--closed");
+
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--opened")) {
+    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add("main-nav--closed");
+  } else {
+    navMain.classList.add("main-nav--opened");
+    navMain.classList.remove("main-nav--closed");
+  }
+});
