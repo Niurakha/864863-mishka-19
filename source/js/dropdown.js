795f835125bae54f3cb1a8af54fe33022222382e
Niurakha <59962207+Niurakha@users.noreply.github.com>
2020-04-26 18:02:39 +0300

    Переписла код js под новую адаптивную разметку

--- source/js/dropdown.js
+++ source/js/dropdown.js
@@ -2,16 +2,26 @@
 
 let navToggle = document.querySelector(".main-nav__toggle");
 let navMain = document.querySelector(".main-nav");
+let userNav = document.querySelector(".user-nav");
 navMain.classList.remove("main-nav--nojs");
 navMain.classList.remove("main-nav--opened");
 navMain.classList.add("main-nav--closed");
+userNav.classList.remove("user-nav--opened");
+userNav.classList.add("user-nav--closed");
 
 navToggle.addEventListener("click", function () {
-  if (navMain.classList.contains("main-nav--opened")) {
+  if (
+    navMain.classList.contains("main-nav--opened") &&
+    userNav.classList.contains("user-nav--opened")
+  ) {
     navMain.classList.remove("main-nav--opened");
     navMain.classList.add("main-nav--closed");
+    userNav.classList.remove("user-nav--opened");
+    userNav.classList.add("user-nav--closed");
   } else {
     navMain.classList.add("main-nav--opened");
     navMain.classList.remove("main-nav--closed");
+    userNav.classList.add("user-nav--opened");
+    userNav.classList.remove("user-nav--closed");
   }
 });
